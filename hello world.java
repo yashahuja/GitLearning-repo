200e6521cec84d362b00709e4c1fe50ec67dc844
Yash Ahuja <Yashahujaofficial@gmail.com>
2021-04-05 16:23:57 +0530

    dev1 update

--- hello world.java
+++ hello world.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String args[]) {
 
         System.out.println("hello world!");
-        System.out.println("hello Universe!");
-        System.out.println("hello galaxy");
-        System.out.println("helloooooooooo!!!!!!!!!!!");
+        System.out.println("hello Universe!!!!!!!!!!!!!!!!!!");
+        System.out.println("trying to learn git");
     }
 
 }
